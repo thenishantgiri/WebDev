6e9c261c71c8dafe5486d6728723c8e42825a1da
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-08 06:24:27 +0530

    js: data types

---
+++ Day06/js-basics/datatypes.js
@@ -0,0 +1,20 @@
+// function a() {
+//     return 1
+//  }
+
+let a = function () {
+    return 1
+}
+
+console.log(1024, typeof 1024)
+console.log(10.24, typeof 10.24)
+console.log('a', typeof 'a')
+console.log("asdf", typeof "asdf")
+console.log(true, typeof true)
+console.log(null, typeof null)
+console.log(undefined, typeof undefined)
+// console.log(a, typeof a)
+// console.log(a(), typeof a())
+
+console.log(a, typeof a)
+console.log(a(), typeof a())
