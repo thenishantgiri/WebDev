4f3e1521eb9e9d9020d201d2cb2e2055ab71b7b4
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-08 20:33:04 +0530

    js: high order functions

--- Day06/js-basics/hof.js
+++ Day06/js-basics/hof.js
@@ -1,30 +1,30 @@
 function greet(name) {
-    console.log('Hello', name)
+  console.log("Hello", name);
 }
+
 function createGreeter(greeting) {
+  function greet(name) {
+    // here we treat as if name is function
+    console.log(greeting, name());
+  }
 
-    function greet(name) {
-        // here we treat as if name is function
-        console.log(greeting, name())
-    }
-
-    return greet
+  return greet;
 }
 
 function getName() {
-    return document.getElementById('namebox').value
+  return document.getElementById("namebox").value;
 }
 
-let g1 = createGreeter('Good Morning')
-let g2 = createGreeter('Good Evening')
+let g1 = createGreeter("Good Morning");
+let g2 = createGreeter("Good Evening");
 
-console.log(typeof g1)
+console.log(typeof g1);
 
-console.log(greet('Arnav'))
-console.log(greet('Prateek'))
+console.log(greet("Arnav"));
+console.log(greet("Prateek"));
 
-console.log(g1('Arnav'))
-console.log(g1('Prateek'))
+console.log(g1("Arnav"));
+console.log(g1("Prateek"));
 
-console.log(g2('Arnav'))
-console.log(g2('Prateek'))
+console.log(g2("Arnav"));
+console.log(g2("Prateek"));
