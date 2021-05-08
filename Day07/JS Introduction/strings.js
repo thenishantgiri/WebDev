a173d9048b912d3918208a983cbae8ddf3ccddb0
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-09 01:35:56 +0530

    js: string methods

--- Day07/JS Introduction/strings.js
+++ Day07/JS Introduction/strings.js
@@ -21,3 +21,17 @@ console.log(s.indexOf(key));
 let str = "ab aab abbcd aab";
 console.log(str.indexOf("ab"));
 console.log(str.lastIndexOf("ab", 16));
+
+//String Methods
+
+let mainStr = "this-is-a-long-string";
+
+// let smallStr = mainStr.slice(2, 3);
+
+// let smallStr = mainStr.substring(2, 10);
+
+// let smallStr = mainStr.slice(-6, -4);
+
+let smallStr = mainStr.substr(-6, 4);
+
+console.log(smallStr);
