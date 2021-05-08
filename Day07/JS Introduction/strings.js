6db94be868a83332414e6662c93634799f0da29a
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-09 01:12:16 +0530

    js: strings

---
+++ Day07/JS Introduction/strings.js
@@ -0,0 +1,23 @@
+//Strings
+
+// let p = "arnav";
+// let str = `this is ${p}'s string`;
+// let longstr = "this is \
+// a very long \n \
+// string";
+// console.log(longstr.length);
+
+//Searching
+
+let s = "this has some data here";
+let key = "has";
+
+console.log(s.indexOf(key));
+
+// let str = "this is a easy job";
+// console.log(str.indexOf("is"));
+// console.log(str.indexOf("is", 4));
+
+let str = "ab aab abbcd aab";
+console.log(str.indexOf("ab"));
+console.log(str.lastIndexOf("ab", 16));
