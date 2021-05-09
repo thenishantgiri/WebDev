833c6f1fd7d20efcb81a5aff9bb0c78310f4bd4b
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-10 02:28:03 +0530

    js: string literals

---
+++ Day07/string-literals/script.js
@@ -0,0 +1,24 @@
+// He said , "This is awesome!"
+
+let s1 = 'He said,"This is awesome!"';
+
+// let s2 = "He said, \"This is awesome!\"";  //use of escape symbol
+
+let s3 = "He said, 'This is awesome!'";
+
+let s4 = `He said, "This is awesome!"`;
+
+let s5 = "This string is \n in multiple lines";
+
+let s6 = `
+    This string is
+    in multiple lines
+`;
+
+let name = "John";
+
+let g1 = "Good Morning " + name;
+
+let g2 = `Good Morning ${name}`;
+
+let s7 = `10 + 2 = ${10 + 2}`;
