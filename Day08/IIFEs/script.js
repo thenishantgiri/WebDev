097447247049c1b4fb66b7107c7c8a7605d36add
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-19 01:07:18 +0530

    JS: IIFEs - fixing For Loop using IIFEs

--- Day08/IIFEs/script.js
+++ Day08/IIFEs/script.js
@@ -2,6 +2,7 @@
 /* 
 Benefits: --> Local Scope / Block Scope
           --> Minification
+          --> For Loops
 
 */
 
@@ -33,3 +34,21 @@ function doMath() {
   l("root(2) = " + r(2));
   l("sin(10) = " + s(10));
 })(console.log, Math.pow, Math.sqrt, Math.sin);
+
+//For Loops
+
+//this For Loop prints "10"x10 times, instead of printing 0-9
+for (var i = 0; i < 10; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 100);
+}
+
+//fixing For Loop using IIFEs
+for (var i = 0; i < 10; i++) {
+  (function (j) {
+    setTimeout(function () {
+      console.log(j);
+    }, 100);
+  })(i);
+}
