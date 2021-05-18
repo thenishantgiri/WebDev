44095cbbd9408b25c8e1fee9137234ba4549bbd5
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-18 23:48:52 +0530

    JS: Promise - Resolve & Reject Parameter

--- Day08/promises/script.js
+++ Day08/promises/script.js
@@ -10,20 +10,30 @@
 //   console.log(data);
 // });
 
-//Promise: Resolve Parameter
-function fakeDownloadPromise() {
+//Promise: Resolve & Reject Parameter
+function fakeDownloadPromise(correct) {
   return new Promise(function (resolve, reject) {
     //return a Promise object
     setTimeout(function () {
-      let downloadedData = "Some data from the Internet";
-      resolve(downloadedData);
+      let downloadedData = "some data from the Internet";
+      if (correct) {
+        resolve(downloadedData);
+      } else {
+        reject(new Error("couldn't download file"));
+      }
     }, 1000);
   });
 }
 
-fakeDownloadPromise().then(function (data) {
-  //this is a promise object, on which we can use "then" call, to write our resolve function
+fakeDownloadPromise(false)
+  .then(function (data) {
+    //this is a promise object, on which we can use "then" call, to write our resolve function
+    //When we call reject, catch function gets executed
 
-  console.log("The data that we downloaded is -->");
-  console.log(data);
-});
+    console.log("The data that we downloaded is -->");
+    console.log(data);
+  })
+  .catch(function (err) {
+    //When we call reject, catch function gets executed
+    console.log(err);
+  });
