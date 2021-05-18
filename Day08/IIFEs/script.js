aa7a4052d82c3119dc31ad354f2036f87b7ffcf6
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-19 01:21:08 +0530

    JS: IIFEs - fixing For Loop using setTimeout() function

--- Day08/IIFEs/script.js
+++ Day08/IIFEs/script.js
@@ -59,3 +59,9 @@ for (let i = 0; i < 10; i++) {
     console.log(i);
   }, 100);
 }
+
+//fixing For Loop (if not using ES6 : ecmascript 6 and want to use var instead of let )
+//Using signature of "setTimeout()", which has a "callback() function", a delay, an array of arguments which are passed to callback() function
+for (var i = 0; i < 10; i++) {
+  setTimeout(console.log, 100, i); //copy of i is passed, because it's "pass by value"
+}
