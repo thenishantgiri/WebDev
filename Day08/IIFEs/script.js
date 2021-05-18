526c7cde2d496f58342f866c3b57ada701e83998
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-19 00:48:55 +0530

    JS: IIFEs - Minification

--- Day08/IIFEs/script.js
+++ Day08/IIFEs/script.js
@@ -1,13 +1,13 @@
 //IIFE --> Immediately Invoked Function Expression
 /* 
 Benefits: --> Local Scope / Block Scope
- 
+          --> Minification
 
 */
 
 //encapsulated the function, and executed it immediately without giving it any call
-//Block Scope
 
+//Block Scope
 (function () {
   var a = 10;
   var b = a / 5;
@@ -15,3 +15,21 @@ Benefits: --> Local Scope / Block Scope
 })();
 
 // console.log(a); //a is not accessible, as IIFEs have block scope
+
+//Minification
+function doMath() {
+  console.log("3^4 = " + Math.pow(3, 4));
+  console.log("4^3 = " + Math.pow(4, 3));
+  console.log("root(2) = " + Math.sqrt(2));
+  console.log("sin(10) = " + Math.sin(10));
+}
+
+// doMath();
+
+//Turning the "doMath" function into an IIFE
+(function (l, p, r, s) {
+  l("3^4 = " + p(3, 4));
+  l("4^3 = " + p(4, 3));
+  l("root(2) = " + r(2));
+  l("sin(10) = " + s(10));
+})(console.log, Math.pow, Math.sqrt, Math.sin);
