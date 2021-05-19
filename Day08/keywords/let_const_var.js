0df393c728560cce38957ade74b3f56233b6c0b1
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-20 03:28:39 +0530

    JS: Scope - let, var and const

--- Day08/keywords/let_const_var.js
+++ Day08/keywords/let_const_var.js
@@ -9,6 +9,77 @@ b++;
 console.log(b);
 
 //const
-const c = 30;
-c++; //const doesn't allow us to reassign any variable
-console.log(c);
+// const c = 30;
+// c++; //const doesn't allow us to reassign any value to that variable
+// console.log(c);
+
+//Object
+const obj = {
+  k1: "asd",
+  k2: 10,
+  k3: false,
+};
+
+obj.k4 = 123;
+console.log(obj.k4);
+
+delete obj.k1;
+console.log(obj.k1);
+
+const arr = [1, 2, 3];
+
+arr.push(5);
+console.log(arr);
+
+//Scope
+/*
+    var --> has a scope of a function
+    let --> has a scope of a block
+    const --> has a block scope
+*/
+
+//var
+// var myVar = 10;
+
+// function alpha() {
+//   var myVar = 11;
+//   if (true) {
+//     var myVar = 21;
+//     console.log(myVar);
+//   }
+//   console.log(myVar);
+// }
+
+// alpha();
+
+//let
+// let myVar = 10;
+
+// function alpha() {
+//   let myVar = 11;
+//   if (true) {
+//     let myVar = 21;
+//     console.log(myVar);
+//   }
+//   console.log(myVar);
+// }
+
+// alpha();
+
+//const
+const myVar = 10;
+
+function alpha() {
+  const myVar = 11;
+  if (true) {
+    const myVar = 21;
+    if (true) {
+      const myVar = 33;
+      console.log(myVar);
+    }
+    console.log(myVar);
+  }
+  console.log(myVar);
+}
+
+alpha();
