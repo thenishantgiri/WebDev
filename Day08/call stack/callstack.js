36e6d959a2af5b7e57c12fdd159e2b19614e9526
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-20 04:53:21 +0530

    JS: callstack

---
+++ Day08/call stack/callstack.js
@@ -0,0 +1,26 @@
+function ceo() {
+  console.log("Let's make a dent in the universe");
+  cto();
+}
+
+function cto() {
+  console.log("Let's make a gamechanging product");
+  vpengg();
+}
+
+function vpengg() {
+  console.log("Let's make a new Javascript framework");
+  techlead();
+}
+
+function techlead() {
+  console.log("Let's port Angular to Typescript");
+  developer();
+}
+
+function developer() {
+  console.log("Let's copy some code from StackOverflow");
+  throw new Error("The code didn't work"); // to see the call stack in output
+}
+
+ceo();
