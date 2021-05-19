e9b91ca48e8135ef3d784c54359ee0d5178e0a46
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-20 03:01:38 +0530

    JS: Keywords - var, let, const

---
+++ Day08/keywords/let_const_var.js
@@ -0,0 +1,14 @@
+//var
+var a = 10;
+a++;
+console.log(a);
+
+//let
+let b = 20;
+b++;
+console.log(b);
+
+//const
+const c = 30;
+c++; //const doesn't allow us to reassign any variable
+console.log(c);
