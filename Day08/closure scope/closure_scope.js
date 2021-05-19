db13db345c2678ea5a06f31930f95abef67cf9f8
thenishantgiri <nishantkrgiri@gmail.com>
2021-05-20 04:35:23 +0530

    JS: Closure Scope

--- Day08/closure scope/closure_scope.js
+++ Day08/closure scope/closure_scope.js
@@ -1,18 +1,51 @@
-function print() {
-  var a = 10;
-  console.log(a);
-}
+// function print() {
+//   var a = 10;
+//   console.log(a);
+// }
 
-// console.log(a); //error : because a was defined inside a function
+// // console.log(a); //error : because a was defined inside a function
 
-print();
+// print();
 
-function count() {
-  var initial = 1;
+// function count() {
+//   var initial = 1;
+//   function plus() {
+//     console.log(++initial);
+//   }
+//   return plus;
+// }
+
+// var c = count();
+// c();
+// c();
+
+function counter(initial) {
+  var val = initial;
   function plus() {
-    console.log(++initial);
+    console.log(++val);
   }
-  plus();
+  return plus;
 }
 
-count();
+var c = counter(10);
+c();
+c();
+
+//creating a function counter
+function createCounter(initVal, deltaVal) {
+  return {
+    up() {
+      initVal += deltaVal;
+      console.log(initVal);
+    },
+    down() {
+      initVal -= deltaVal;
+      console.log(initVal);
+    },
+  };
+}
+
+var c = createCounter(10, 2);
+c.up();
+c.up();
+c.down();
