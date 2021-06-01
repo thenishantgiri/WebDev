47228e6d93997a1151e62ccec4c743b1ab89781b
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-06-01 21:36:54 +0530

    JS : Promise - deferred resolve

--- Day11/deferred_resolve.js
+++ Day11/deferred_resolve.js
@@ -1,6 +1,6 @@
 function downloadPromise() {
   return new Promise(function (resolve, reject) {
-    console.log("Startint to download the file...");
+    console.log("Starting to download the file...");
     setTimeout(function () {
       console.log("Download is completed.");
       resolve();
@@ -8,6 +8,20 @@ function downloadPromise() {
   });
 }
 
-downloadPromise().then(function () {
+/**
+ * 
+ * downloadPromise().then(function () {
   console.log("**After Download**");
 });
+ *
+ * If you want the promise result sometime later, follow below steps
+ * 
+ */
+
+let downloadedFile = downloadPromise();
+
+setTimeout(function () {
+  downloadedFile.then(function () {
+    console.log("**After Download**");
+  });
+}, 5000);
