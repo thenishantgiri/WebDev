2f8866653044d34e79bd2e9964c9664a3aab7e01
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-06-05 05:45:42 +0530

    DOM Manipulation : Todo List (order shift)

--- Day14/DOM_PLAINJS/todolist/todo.js
+++ Day14/DOM_PLAINJS/todolist/todo.js
@@ -5,7 +5,6 @@ window.onload = () => {
 
   addtask.onclick = () => {
     let li = document.createElement("li");
-    li.innerText = newtask.value;
 
     //delete button
     let xBtn = document.createElement("button");
@@ -16,7 +15,44 @@ window.onload = () => {
       event.target.parentElement.remove();
     };
 
+    //up button
+    let upBtn = document.createElement("button");
+    upBtn.innerText = "⬆️";
+
+    upBtn.onclick = (event) => {
+      // event.target = the up button
+      // event.target.parentElement = the <li> item
+      // event.target.parentElement.parentElement is the todolist
+
+      event.target.parentElement.parentElement.insertBefore(
+        event.target.parentElement, //insert this element
+        event.target.parentElement.previousElementSibling //before it's previous element
+      );
+    };
+
+    //down button
+    let downBtn = document.createElement("button");
+    downBtn.innerText = "⬇️";
+
+    downBtn.onclick = (event) => {
+      // event.target = the up button
+      // event.target.parentElement = the <li> item
+      // event.target.parentElement.parentElement is the todolist
+
+      event.target.parentElement.parentElement.insertBefore(
+        event.target.parentElement.nextElementSibling, //insert next element
+        event.target.parentElement //before current element
+      );
+    };
+
+    //Add the task text
+    let taskSpan = document.createElement("span");
+    taskSpan.innerText = newtask.value;
+
     li.appendChild(xBtn);
+    li.appendChild(upBtn);
+    li.appendChild(downBtn);
+    li.appendChild(taskSpan);
 
     todolist.appendChild(li);
 
