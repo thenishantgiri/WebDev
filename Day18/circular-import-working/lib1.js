179ab84b4b8cf70b2dccbd52ab68c47e8f908180
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-06-16 05:19:42 +0530

    ExpressJS : NodeJS - circular dependencies properly working

--- Day18/circular-import-working/lib1.js
+++ Day18/circular-import-working/lib1.js
@@ -1,2 +1,5 @@
 module.exports.a = 10;
 module.exports.lib2 = require("./lib2");
+module.exports.fun = function () {
+  console.log("This is fun function inside lib1");
+};
--- Day18/circular-import-working/main.js
+++ Day18/circular-import-working/main.js
@@ -6,3 +6,8 @@ console.log(lib2);
 
 console.log(lib1.lib2);
 console.log(lib2.lib1);
+
+lib1.lib2.lib1.fun();
+
+//In this working circular export-require scenario,we are just adding data to the existing "module.export object";
+//unlike in non - working circular export -require scenario, where we're creating a new object and assigning it to "module.exports"
