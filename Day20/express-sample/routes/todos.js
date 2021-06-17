793c91518b331248392d4275e8093ef55d895bed
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-06-18 01:21:10 +0530

    Expess Routing and AJAX : basics of api - router folder

---
+++ Day20/express-sample/routes/todos.js
@@ -0,0 +1,17 @@
+const route = require("express").Router();
+
+let todos = [];
+
+route.get("/", (req, res) => {
+  res.send(todos);
+});
+
+route.post("/", (req, res) => {
+  todos.push({
+    task: req.body.task,
+  });
+
+  res.send(todos);
+});
+
+module.exports = route;
