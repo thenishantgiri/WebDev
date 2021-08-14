f1705affa59b8aad7832e820fba9189043142ffa
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-08-15 05:23:15 +0530

    Socket.IO Client Library

--- Day25/SOCKET_IO/server.js
+++ Day25/SOCKET_IO/server.js
@@ -7,6 +7,10 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on("connection", (socket) => {
+  console.log("connected with socket id = ", socket.id);
+});
+
 app.use("/", express.static(__dirname + "/public"));
 
 server.listen(3344, () => {
